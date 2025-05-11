0cb1adf939906185e4f8ab17f86024a68b53a0c6
WanGiB <wannclem@gmail.com>
2025-05-11 10:07:51 +0100

    cleanups: Tidy field manipulators

--- packages/mongo_document_generator/lib/src/utils/field_manipulators.dart
+++ packages/mongo_document_generator/lib/src/utils/field_manipulators.dart
@@ -1,6 +1,5 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:mongo_document_annotation/mongo_document_annotation.dart';
 import 'package:source_gen/source_gen.dart';
 
 FieldRename? getFieldRenamePolicy(
