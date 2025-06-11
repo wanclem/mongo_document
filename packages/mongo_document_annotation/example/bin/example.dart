e5cfebda88588b56730ccf99eded29955c657684
WanGiB <wannclem@gmail.com>
2025-06-11 09:43:43 +0100

    Update samples

--- packages/mongo_document_annotation/example/bin/example.dart
+++ packages/mongo_document_annotation/example/bin/example.dart
@@ -1,22 +1,29 @@
 import 'package:example/env/dump.dart';
+import 'package:example/models/post.dart';
+import 'package:example/models/user.dart';
 import 'package:mongo_document_annotation/mongo_document_annotation.dart';
 
 Future<void> main() async {
   Environment env = Environment();
   String mongoUri = env.get('MONGO_URI') ?? "";
-  await MongoDbConnection.initialize(mongoUri);
-  //Create a new user
-  // User? newUser = await User(firstName: "John", lastName: "Doe").save();
-  // print("User Created: $newUser");
-  // //Create a new post
-  // Post? post =
-  //     await Post(
-  //       author: newUser,
-  //       body: "Setting up my post",
-  //       tags: ['setup', 'init'],
-  //     ).save();
-  // print("Post Created: $post");
-  // final post = await Posts.findById("649f2a0b8c1d4e3f8c5b6e7a");
-  // print(post);
-}
 
+  ///Initialize connection to the database once on application start
+  await MongoDbConnection.initialize(mongoUri);
+
+  // Create a new user
+  User? newUser = await User(firstName: "John", lastName: "Doe").save();
+  print("User Created: $newUser");
+
+  //Create a new post
+  Post? post =
+      await Post(
+        author: newUser,
+        body: "Hello World",
+        tags: ['setup', 'init', 'starter'],
+      ).save();
+
+  print("Post Created: $post");
+
+  final existingPost = await Posts.findById("649f2a0b8c1d4e3f8c5b6e7a");
+  print(existingPost);
+}
